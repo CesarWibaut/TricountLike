5a49f54fe06fb01b9d74e6bcef579a36f6a3b4d2
Cesar WIBAUT <wibautc@meleze21.iut-infobio.priv.univ-lille1.fr>
2019-01-09 09:14:49 +0100

    Script v1 done

--- script.sql
+++ script.sql
@@ -1,11 +1,13 @@
-DROP TABLE USER;
+DROP TABLE SPENT_FOR;
+DROP TABLE PARTICIPATE;
+DROP TABLE SPENT;
+DROP TABLE USERS;
 DROP TABLE NOT_USER;
 DROP TABLE EVENT;
-DROP TABLE SPENT;
 
-CREATE TABLE USER (uno SERIAL,
+CREATE TABLE USERS (uno SERIAL,
                    login text not null,
-                   password text not null,
+                   pwd text not null,
                    lastname text,
                    firstname text,
                    CONSTRAINT pk_user PRIMARY KEY(uno));
@@ -14,11 +16,23 @@ CREATE TABLE NOT_USER (name text not null);
 
 CREATE TABLE EVENT (eno SERIAL,
                     title text not null,
-                    desc text not null,
+                    descr text not null,
                     CONSTRAINT pk_event PRIMARY KEY(eno));
 
 CREATE TABLE SPENT (sno SERIAL,
+					uno INTEGER,
                     createdAt date not null,
-                    desc text not null,
-                    CONSTRAINT pk_spent PRIMARY KEY(sno));
+                    descr text not null,
+                    CONSTRAINT pk_spent PRIMARY KEY(sno),
+					CONSTRAINT fk_user3 FOREIGN KEY(uno) REFERENCES USERS(uno));
+
+CREATE TABLE PARTICIPATE (uno INTEGER,
+						  eno INTEGER,
+						 CONSTRAINT fk_user FOREIGN KEY (uno) REFERENCES USERS(uno),
+						 CONSTRAINT fk_event FOREIGN KEY (eno) REFERENCES EVENT(eno));
+
+CREATE TABLE SPENT_FOR (uno INTEGER,
+						sno INTEGER,
+						CONSTRAINT fk_user2 FOREIGN KEY (uno) REFERENCES USERS(uno),
+					    CONSTRAINT fk_spent FOREIGN KEY (sno) REFERENCES SPENT(sno));
 
